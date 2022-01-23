Fix: Cross entropy loss - removed softmax

models/common.py
@@ -8,28 +8,27 @@ from models.resnet import ResNet18, ResNet34, ResNet50, ResNet101, ResNet152
 from models.mobilenets import MobileNetV1, MobileNetV2
 
 
-def get_model(arch, num_classes, input_shape, channels=3):
+def get_model(arch, num_classes, channels=3):
     """
     Args:
         arch: string, Network architecture
         num_classes: int, Number of classes
-        input_shape: list/tuple, Input shape of the network
         channels: int, Number of input channels
     Returns:
         model, nn.Module, generated model
     """
     if arch == "CustomNet":
-        model = CustomNet(num_classes, channels)
+        model = CustomNet(channels, num_classes)
     elif arch.lower() == "resnet18":
-        model = ResNet18(num_classes, input_shape, channels)
+        model = ResNet18(channels, num_classes)
     elif arch.lower() == "resnet34":
-        model = ResNet34(num_classes, input_shape, channels)
+        model = ResNet34(channels, num_classes)
     elif arch.lower() == "resnet50":
-        model = ResNet50(num_classes, input_shape, channels)
+        model = ResNet50(channels, num_classes)
     elif arch.lower() == "resnet101":
-        model = ResNet101(num_classes, input_shape, channels)
+        model = ResNet101(channels, num_classes)
     elif arch.lower() == "resnet152":
-        model = ResNet152(num_classes, input_shape, channels)
+        model = ResNet152(channels, num_classes)
     elif arch.lower() == "mobilenet_v1":
         model = MobileNetV1(num_classes, channels)
     elif arch.lower() == "mobilenet_v2":
@@ -51,7 +50,6 @@ def save_model(model, epoch, batches, ckpt_dir, accuracy, best_accuracy, logger)
         best_accuracy: Best accuracy
         logger: logger
     """
-    ckpt_dir = os.path.join(ckpt_dir, "checkpoints")
     if not os.path.isdir(ckpt_dir):
         os.makedirs(ckpt_dir)
     ckpt_path = os.path.join(ckpt_dir, "model_epoch_" + str(epoch) + "_batch_" + str(batches) + ".pt")
@@ -67,18 +65,17 @@ def save_model(model, epoch, batches, ckpt_dir, accuracy, best_accuracy, logger)
         copyfile(ckpt_path, best_ckpt_path)
 
 
-def load_model(arch, num_classes, input_shape, device, model_path='', channels=3, logger=None):
+def load_model(arch, num_classes, device, model_path='', channels=3, logger=None):
     """
     Args:
         arch: str, Network architectire (See documentation for supported architectures)
         num_classes: int, Number of classes
         device: device_id
         model_path: str, Path to model
-        input_shape: list/tuple, Input shape of the network
         channels: int, Number of input channels
     """
-
-    model = get_model(arch, num_classes, input_shape, channels)
+    print("Loading model to device: ", device)
+    model = get_model(arch, num_classes, channels)
     if model_path:
         logger.info(f"Loading weights from {model_path}...")
         model.load_state_dict(torch.load(model_path))

models/resnet.py
@@ -72,8 +72,10 @@ class ResBlock3(nn.Module):
 
 class ResNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, channels, num_classes):
         super().__init__()
+        self.channels = channels
+        self.num_classes = num_classes
 
     def get_block(self, in_channel, out_channels, stride, num_blocks):
         num_conv = len(out_channels)
@@ -98,74 +100,72 @@ class ResNet(nn.Module):
         x = self.block3(x)
         x = self.block4(x)
         x = self.block5(x)
-        x = F.avg_pool2d(x, 4)
+        x = F.avg_pool2d(x, x.shape[-1])
         x = x.reshape(x.shape[0], -1)
-        # x = x.view(x.size()[0], -1)
-        x = self.fc1(x)
-        x = F.log_softmax(x, dim=1)
+        x = self.fc(x)
         return x
 
 
 class ResNet18(ResNet):
 
-    def __init__(self, num_classes, input_shape, channels=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(channels, 64, 7, 2, padding=get_padd(7))
+    def __init__(self, channels, num_classes):
+        super().__init__(channels, num_classes)
+        self.conv1 = nn.Conv2d(self.channels, 64, 7, 2, padding=get_padd(7))
         self.bn1 = nn.BatchNorm2d(64)
         self.block2 = self.get_block(in_channel=64, out_channels=[64, 64], stride=1, num_blocks=2)
         self.block3 = self.get_block(in_channel=64, out_channels=[128, 128], stride=2, num_blocks=2)
         self.block4 = self.get_block(in_channel=128, out_channels=[256, 256], stride=2, num_blocks=2)
         self.block5 = self.get_block(in_channel=256, out_channels=[512, 512], stride=2, num_blocks=2)
-        self.fc1 = nn.Linear((input_shape[0] // (2 ** 7)) * (input_shape[1] // (2 ** 7)) * 512, num_classes)
+        self.fc = nn.Linear(512, self.num_classes)
 
 
 class ResNet34(ResNet):
 
-    def __init__(self, num_classes, input_shape, channels=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(channels, 64, 7, 2, padding=get_padd(7))
+    def __init__(self, channels, num_classes):
+        super().__init__(channels, num_classes)
+        self.conv1 = nn.Conv2d(self.channels, 64, 7, 2, padding=get_padd(7))
         self.bn1 = nn.BatchNorm2d(64)
         self.block2 = self.get_block(in_channel=64, out_channels=[64, 64], stride=1, num_blocks=3)
         self.block3 = self.get_block(in_channel=64, out_channels=[128, 128], stride=2, num_blocks=4)
         self.block4 = self.get_block(in_channel=128, out_channels=[256, 256], stride=2, num_blocks=6)
         self.block5 = self.get_block(in_channel=256, out_channels=[512, 512], stride=2, num_blocks=4)
-        self.fc1 = nn.Linear((input_shape[0] // (2 ** 7)) * (input_shape[1] // (2 ** 7)) * 512, num_classes)
+        self.fc = nn.Linear(512, self.num_classes)
 
 
 class ResNet50(ResNet):
-    def __init__(self, num_classes, input_shape, channels=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(channels, 64, 7, 2, padding=get_padd(7))
+    def __init__(self, channels, num_classes):
+        super().__init__(channels, num_classes)
+        self.conv1 = nn.Conv2d(self.channels, 64, 7, 2, padding=get_padd(7))
         self.bn1 = nn.BatchNorm2d(64)
         self.block2 = self.get_block(in_channel=64, out_channels=[64, 64, 256], stride=1, num_blocks=3)
         self.block3 = self.get_block(in_channel=256, out_channels=[128, 128, 512], stride=2, num_blocks=4)
         self.block4 = self.get_block(in_channel=512, out_channels=[256, 256, 1024], stride=2, num_blocks=6)
         self.block5 = self.get_block(in_channel=1024, out_channels=[512, 512, 2048], stride=2, num_blocks=3)
-        self.fc1 = nn.Linear((input_shape[0] // (2 ** 7)) * (input_shape[1] // (2 ** 7)) * 2048, num_classes)
+        self.fc = nn.Linear(2048, self.num_classes)
 
 
 class ResNet101(ResNet):
-    def __init__(self, num_classes, input_shape, channels=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(channels, 64, 7, 2, padding=get_padd(7))
+    def __init__(self, channels, num_classes):
+        super().__init__(channels, num_classes)
+        self.conv1 = nn.Conv2d(self.channels, 64, 7, 2, padding=get_padd(7))
         self.bn1 = nn.BatchNorm2d(64)
         self.block2 = self.get_block(in_channel=64, out_channels=[64, 64, 256], stride=1, num_blocks=3)
         self.block3 = self.get_block(in_channel=256, out_channels=[128, 128, 512], stride=2, num_blocks=4)
         self.block4 = self.get_block(in_channel=512, out_channels=[256, 256, 1024], stride=2, num_blocks=23)
         self.block5 = self.get_block(in_channel=1024, out_channels=[512, 512, 2048], stride=2, num_blocks=3)
-        self.fc1 = nn.Linear((input_shape[0] // (2 ** 7)) * (input_shape[1] // (2 ** 7)) * 2048, num_classes)
+        self.fc = nn.Linear(2048, self.num_classes)
 
 
 class ResNet152(ResNet):
-    def __init__(self, num_classes, input_shape, channels=3):
-        super().__init__()
-        self.conv1 = nn.Conv2d(channels, 64, 7, 2, padding=get_padd(7))
+    def __init__(self, channels, num_classes):
+        super().__init__(channels, num_classes)
+        self.conv1 = nn.Conv2d(self.channels, 64, 7, 2, padding=get_padd(7))
         self.bn1 = nn.BatchNorm2d(64)
         self.block2 = self.get_block(in_channel=64, out_channels=[64, 64, 256], stride=1, num_blocks=3)
         self.block3 = self.get_block(in_channel=256, out_channels=[128, 128, 512], stride=2, num_blocks=8)
         self.block4 = self.get_block(in_channel=512, out_channels=[256, 256, 1024], stride=2, num_blocks=36)
         self.block5 = self.get_block(in_channel=1024, out_channels=[512, 512, 2048], stride=2, num_blocks=3)
-        self.fc1 = nn.Linear((input_shape[0] // (2 ** 7)) * (input_shape[1] // (2 ** 7)) * 2048, num_classes)
+        self.fc = nn.Linear(2048, self.num_classes)
 
 
 if __name__ == "__main__":

train.py
@@ -23,7 +23,7 @@ class Train():
         self.ckpt_dir = config["Logging"]["ckpt_dir"]
         self.eval_per_epoch = config["Train"]["eval_per_epoch"]
         self.device = get_device(config["Train"]["device"])
-        self.model = load_model(config["Train"]["arch"], self.num_classes, self.input_shape, self.device,
+        self.model = load_model(config["Train"]["arch"], self.num_classes, self.device,
                                 config["Train"]["pretrained"], self.channels, self.logger)
         self.optimizer = self.get_optimizer(config["Train"]["optimizer"], config["Train"]["lr_scheduler"]["lr_init"])
         self.scheduler = self.get_scheduler(config["Train"]["lr_scheduler"])
@@ -72,7 +72,7 @@ class Train():
         total_num_params = 0
         for p in params:
             total_num_params += p
-        self.logger.info(f"Number of parameters: {total_num_params}")
+        self.logger.info(f"Number of parameters of the model: {total_num_params}")
 
         global_step = 0
         eval_steps = list(np.linspace(len(self.train_loader) // self.eval_per_epoch, len(self.train_loader),
@@ -91,7 +91,7 @@ class Train():
                 y_pred = self.model(X_train)
                 train_loss = self.criterion(y_pred, y_train)
 
-                global_step += self.batch_size
+                global_step += 1
 
                 self.optimizer.zero_grad()
                 train_loss.backward()
@@ -132,6 +132,7 @@ class Train():
 
         self.logger.info(f"---------------- Training Finished ----------------")
 
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -142,7 +143,6 @@ if __name__ == "__main__":
     # Get parameters from the config file
     with open(args.config, 'r') as cfg_file:
         config = yaml.load(cfg_file, Loader=yaml.FullLoader)
-
     copy_config(config)
 
     Trainer = Train(config)

utils/dataset.py
@@ -5,6 +5,7 @@ import random
 import imutils
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.functional import one_hot
 
 
 class RandomCrop(object):
@@ -80,6 +81,7 @@ class DatasetBuilder(Dataset):
         self.labels = self.load_labels(logger)
         self.img_size = tuple(img_size)
         self.classes = self.get_classes(classes_path)
+        self.num_classes = len(self.classes)
         self.augment = augment
 
     def __len__(self):
@@ -88,7 +90,8 @@ class DatasetBuilder(Dataset):
     def __getitem__(self, item):
         label = self.labels[item]
         img_path = label["img_path"]
-        class_id = label["label"]
+        class_id = torch.tensor(label["label"])
+        # class_one_hot = one_hot(class_id, self.num_classes)
         img = self.read_img(img_path)
         if self.augment:
             img = self.augment_img(img)
@@ -125,19 +128,26 @@ class DatasetBuilder(Dataset):
         return img
 
     def preprocess_img(self, img, img_path):
-        try:
-            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
-            img = cv.resize(img, self.img_size)
-            img = np.transpose(img, (2, 0, 1))
-            img = img / 255.
-        except:
-            print(img_path)
+        img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+        org_height, org_width, _ = img.shape
+        # Pad image if dimensions of the image are smaller than provided image size
+        if org_height < self.img_size[1] and org_width < self.img_size[0]:
+            pad_val = [(int((self.img_size[1] - org_height) / 2.),
+                        int((self.img_size[1] - org_height) / 2.)),
+                       (int((self.img_size[0] - org_width) / 2.),
+                        int((self.img_size[0] - org_width) / 2.)),
+                       (0, 0)]
+            img = np.pad(img, pad_val)
+        img = cv.resize(img, self.img_size)
+        img = np.transpose(img, (2, 0, 1))
+        img = img / 255.
         return torch.FloatTensor(img)
 
     @staticmethod
     def get_classes(classes_path):
         with open(classes_path, 'r') as f:
             classes = f.read().split('\n')
+        classes = [cl for cl in classes if cl]
         return classes
 
 