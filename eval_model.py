a087ec2f9fa6ea93659d9df6d18871497cc96594
micon141 <micon141@gmail.com>
2022-01-26 21:56:24 +0100

    Ref: Evaluation - get accuracy for each class

--- eval_model.py
+++ eval_model.py
@@ -2,14 +2,23 @@ import os
 import argparse
 import yaml
 import torch
+from torch import nn
 from tqdm import tqdm
+from _collections import defaultdict
 
 from utils.dataset import DatasetBuilder
 from utils.utils import get_device, get_logger
 from models.common import load_model
 
 
-def evaluate(model, dataset, device):
+def get_classes(classes_path):
+    with open(classes_path, 'r') as f:
+        classes = f.read().split('\n')
+    classes = [cl for cl in classes if cl]
+    return classes
+
+
+def evaluate(model, dataset, device, classes=None):
     """ Get predictions from model and calculate accuracy
     Args:
         model: PyTorch Model
@@ -18,17 +27,34 @@ def evaluate(model, dataset, device):
     Return:
 
     """
-
+    if classes:
+        classes_accuracy = {}
+        for cl_name in classes:
+            classes_accuracy[cl_name] = {"correct": 0, "examples": 0}
     correct = 0
     for num in tqdm(range(len(dataset))):
         img, label = dataset[num]
         img = img.unsqueeze(0).cuda(device)
         pred = model(img)
+        pred = nn.Softmax()(pred)
         predicted = int(torch.max(pred.data, 1)[1])
+        # print(label)
+        # print(predicted)
         if predicted == label:
             correct += 1
+        if classes:
+            classes_accuracy[classes[int(label)]]["examples"] += 1
+            if predicted == label:
+                classes_accuracy[classes[int(label)]]["correct"] += 1
+        # if num > 5:
+        #     break
     acc = int(correct / len(dataset) * 100)
-    print(f"Accuracy: {acc} %")
+    print(f"Total Accuracy: {acc} %")
+    if classes:
+        print("Accuracy by classes")
+        for cl_name, val in classes_accuracy.items():
+            acc = int(val["correct"] / val["examples"] * 100)
+            print(f"{cl_name}: {acc}%")
 
 
 if __name__ == "__main__":
@@ -42,6 +68,8 @@ if __name__ == "__main__":
                         help="Path to config file")
     parser.add_argument("--device", type=str, default='0',
                         help="Device: 'cpu', '0', '1', ...")
+    parser.add_argument("--accuracy-by-class", action="store_true",
+                        help="Display accuracy for each class")
     args = parser.parse_args()
 
     if os.path.isfile(args.config):
@@ -61,4 +89,9 @@ if __name__ == "__main__":
     else:
         raise ValueError(f"Path {args.dataset} not found")
 
-    evaluate(model, dataset, device)
+    if args.accuracy_by_class:
+        classes = get_classes(config["Dataset"]["classes_path"])
+    else:
+        classes = None
+
+    evaluate(model, dataset, device, classes)
